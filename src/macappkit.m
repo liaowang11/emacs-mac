a9713e7931a2789a2315d2d95d4a995dd387cba9
wliao <lwlw1988@gmail.com>
2025-03-17 13:18:47 +0800

    Add support for undecorate-round window style

--- src/macappkit.m
+++ src/macappkit.m
@@ -22,6 +22,7 @@ along with GNU Emacs Mac port.  If not, see <https://www.gnu.org/licenses/>.  */
 
 #include "macterm.h"
 
+#include <AppKit/AppKit.h>
 #include <sys/socket.h>
 
 #include "character.h"
@@ -2617,6 +2618,11 @@ mac_with_suppressed_transparent_titlebar( NSWindow* window, BOOL assumeTranspare
   else
     windowStyle = NSWindowStyleMaskBorderless;
 
+  if (FRAME_UNDECORATED_ROUND (f))
+    {
+      windowStyle |= NSFullSizeContentViewWindowMask;
+    }
+
   if (oldWindow == nil)
     {
       NSScreen *screen = nil;
@@ -2748,6 +2754,14 @@ mac_with_suppressed_transparent_titlebar( NSWindow* window, BOOL assumeTranspare
       [window setIgnoresMouseEvents:YES];
       [window setExcludedFromWindowsMenu:YES];
       window.animationBehavior = NSWindowAnimationBehaviorNone;
+    }
+  if (FRAME_UNDECORATED_ROUND (f))
+    {
+      [window setTitlebarAppearsTransparent:YES];
+      [window setTitleVisibility:NSWindowTitleHidden];
+      [[window standardWindowButton:NSWindowCloseButton] setHidden:YES];
+      [[window standardWindowButton:NSWindowMiniaturizeButton] setHidden:YES];
+      [[window standardWindowButton:NSWindowZoomButton] setHidden:YES];
     }
 }
 
@@ -4323,10 +4337,10 @@ mac_with_suppressed_transparent_titlebar( NSWindow* window, BOOL assumeTranspare
 - (void)updateWindowStyle
 {
   BOOL shouldBeTitled = self.shouldBeTitled;
+  struct frame *f = emacsFrame;
 
   if (emacsWindow.hasTitleBar != shouldBeTitled)
     {
-      struct frame *f = emacsFrame;
       Lisp_Object tool_bar_lines = get_frame_param (f, Qtool_bar_lines);
 
       if (FIXNUMP (tool_bar_lines) && XFIXNUM (tool_bar_lines) > 0)
@@ -4340,6 +4354,11 @@ mac_with_suppressed_transparent_titlebar( NSWindow* window, BOOL assumeTranspare
 	    gui_set_frame_parameters (f, list1 (Fcons (Qtool_bar_lines,
 						       tool_bar_lines)));
 	  });
+      [self setupWindow];
+    }
+
+  if (([emacsWindow styleMask] & NSFullSizeContentViewWindowMask) != FRAME_UNDECORATED_ROUND (f))
+    {
       [self setupWindow];
     }
 
--- src/nsterm.m
+++ src/nsterm.m
@@ -1996,6 +1996,44 @@ ns_set_undecorated (struct frame *f, Lisp_Object new_value, Lisp_Object old_valu
 }
 
 void
+ns_set_undecorated_round (struct frame *f, Lisp_Object new_value, Lisp_Object old_value)
+/* --------------------------------------------------------------------------
+     Set frame F's `undecorated_round' parameter.  If non-nil, F's window-system
+     window is drawn without decorations, title, minimize/maximize boxes
+     and external round borders.  This usually means that the window cannot be
+     dragged, resized, iconified, maximized or deleted with the mouse.  If
+     nil, draw the frame with all the elements listed above unless these
+     have been suspended via window manager settings.
+   -------------------------------------------------------------------------- */
+{
+  NSTRACE ("ns_set_undecorated_round");
+
+  if (!EQ (new_value, old_value))
+    {
+      EmacsView *view = (EmacsView *)FRAME_NS_VIEW (f);
+      NSWindow *oldWindow = [view window];
+      NSWindow *newWindow;
+
+      block_input ();
+
+      FRAME_UNDECORATED_ROUND (f) = !NILP (new_value);
+
+      newWindow = [[EmacsWindow alloc] initWithEmacsFrame:f];
+
+      if ([oldWindow isKeyWindow])
+        [newWindow makeKeyAndOrderFront:NSApp];
+
+      [newWindow setIsVisible:[oldWindow isVisible]];
+      if ([oldWindow isMiniaturized])
+        [newWindow miniaturize:NSApp];
+
+      [oldWindow close];
+
+      unblock_input ();
+    }
+}
+
+void
 ns_set_parent_frame (struct frame *f, Lisp_Object new_value, Lisp_Object old_value)
 /* --------------------------------------------------------------------------
      Set frame F's `parent-frame' parameter.  If non-nil, make F a child
@@ -9749,6 +9787,10 @@ static void cancel_ns_deferred_UAZoomChangeFocus_timer ()
 		 | NSWindowStyleMaskResizable
 		 | NSWindowStyleMaskMiniaturizable
 		 | NSWindowStyleMaskClosable);
+  if (FRAME_UNDECORATED_ROUND (f))
+    {
+      styleMask |= NSFullSizeContentViewWindowMask;
+    }
 
   last_drag_event = nil;
 
@@ -9833,6 +9875,15 @@ static void cancel_ns_deferred_UAZoomChangeFocus_timer ()
 #endif
     }
 
+  if (FRAME_UNDECORATED_ROUND (f))
+    {
+      [self setTitlebarAppearsTransparent:YES];
+      [self setTitleVisibility:NSWindowTitleHidden];
+      [[self standardWindowButton:NSWindowCloseButton] setHidden:YES];
+      [[self standardWindowButton:NSWindowMiniaturizeButton] setHidden:YES];
+      [[self standardWindowButton:NSWindowZoomButton] setHidden:YES];
+    }
+
   return self;
 }
 
@@ -9840,6 +9891,7 @@ static void cancel_ns_deferred_UAZoomChangeFocus_timer ()
 - (void)createToolbar: (struct frame *)f
 {
   if (FRAME_UNDECORATED (f)
+      || FRAME_UNDECORATED_ROUND (f)
       || [self styleMask] == NSWindowStyleMaskBorderless
       || !FRAME_EXTERNAL_TOOL_BAR (f)
       || [self toolbar] != nil)
